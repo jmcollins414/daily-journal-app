801bbc39c454431d1fefbd9d0825c1a12798295b
jmcollins414 <69184929+jmcollins414@users.noreply.github.com>
2020-10-08 23:22:07 -0500

    initial comitt

---
+++ index.js
@@ -0,0 +1,54 @@
+const addEntry = document.querySelector('#entry-form');
+let entryLinkArr = [];
+let entryDivArr = [];
+let count = 1;
+addEntry.addEventListener('submit',createEntry)
+
+function createEntry(){
+    const entryField =  document.querySelector('#entry-textbox');
+    const entryText = document.querySelector('#entry-textbox').value;
+    const  entryContainer = document.querySelector('#entry-container');
+    const  entryDate = document.querySelector("#entry-date").value;
+
+    let entryDiv = document.createElement('div');
+    entryDiv.classList.add('entry-div');
+    entryContainer.append(entryDiv);
+
+    let dateDiv = document.createElement('div');
+    dateDiv.classList.add('date-div')
+    let entryId = "e"+entryDate + count;
+    entryDiv.setAttribute('id',entryId);
+    dateDiv.textContent = entryDate;
+    entryDiv.appendChild(dateDiv)
+
+    let textDiv = document.createElement('div');
+    textDiv.textContent = entryText;
+    entryDiv.appendChild(textDiv);
+    let removeImg = document.createElement('img');
+    removeImg.setAttribute('src','times-circle-regular.svg');
+    removeImg.classList.add('remove-img');
+    entryDiv.appendChild(removeImg);
+
+        let entryLink = document.createElement('a');
+        entryLink.classList.add('entry-link');
+        let entryRef = '#' + 'e' + entryDate + count;
+        entryLink.href = entryRef;
+        let linkName = entryDate
+        entryLink.textContent = linkName;
+        let navContainer = document.querySelector('#entry-nav');
+        navContainer.appendChild(entryLink);
+        entryDivArr.push("#" + entryId);
+    entryLinkArr.push(entryRef);
+   
+    clearTextArea()
+}
+    
+
+
+function clearTextArea(){
+    document.querySelector('#entry-form').reset();
+    count += 1;
+    
+}
+
+
