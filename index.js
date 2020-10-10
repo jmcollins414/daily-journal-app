eb47cff05f492cabbb55e00da0e2558e4d195c0c
jmcollins414 <69184929+jmcollins414@users.noreply.github.com>
2020-10-09 22:36:29 -0500

    altered css grid and made elements more responsive

--- index.js
+++ index.js
@@ -10,30 +10,22 @@ function createEntry(){
     const  entryContainer = document.querySelector('#entry-container');
     const  entryDate = document.querySelector("#entry-date").value;
 
-    
-
-
-
     let entryDiv = document.createElement('div');
     entryDiv.classList.add('entry-div');
     entryContainer.append(entryDiv);
 
     let dateDiv = document.createElement('div');
-    dateDiv.classList.add('date-div')
+    dateDiv.classList.add('date-div');
     let entryId = "e"+ entryDate + count;
     entryDiv.setAttribute('id',entryId);
     dateDiv.textContent = entryDate;
-    entryDiv.appendChild(dateDiv)
+    entryDiv.appendChild(dateDiv);
 
     let textDiv = document.createElement('div');
     textDiv.classList.add('text-div');
     textDiv.textContent = entryText;
     entryDiv.appendChild(textDiv);
-    let removeImg = document.createElement('img');
-    removeImg.setAttribute('src','times-circle-regular.svg');
-    removeImg.setAttribute('alt','remove entry');
-    removeImg.classList.add('remove-img');
-    entryDiv.appendChild(removeImg);
+   
 
         let entryLink = document.createElement('a');
         entryLink.classList.add('entry-link');
@@ -43,8 +35,8 @@ function createEntry(){
         entryLink.textContent = linkName;
         let navContainer = document.querySelector('#entry-nav');
         
-        navContainer.appendChild(entryLink);
-        entryDivArr.push("#" + entryId);
+    navContainer.appendChild(entryLink);
+    entryDivArr.push("#" + entryId);
     entryLinkArr.push(entryRef);
    
     clearTextArea()
@@ -58,8 +50,8 @@ function createEntry(){
 function clearTextArea(){
     document.querySelector('#entry-form').reset();
     count += 1;
-    let LinksHeader = document.querySelector('h2');
-    LinksHeader.style.visibility = "visible";
+    // let LinksHeader = document.querySelector('h2');
+    // LinksHeader.style.visibility = "visible";
 }
 
 
